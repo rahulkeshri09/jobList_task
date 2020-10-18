6f30ab152511cf1a2a71da355a8f05fe5446fd76
rahul <rahulkeshri09.rk@gmail.com>
2020-10-18 23:51:34 +0530

    Updates

---
+++ precache-manifest.a3744bd55a5ce94afcc78946b1742b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b59fc39970abc5c82f20acacee34240",
+    "url": "/jobList_task/index.html"
+  },
+  {
+    "revision": "439c916b57ab3f7a48ba",
+    "url": "/jobList_task/static/css/main.bc05addd.chunk.css"
+  },
+  {
+    "revision": "476a6be6f078d6926aba",
+    "url": "/jobList_task/static/js/2.431facc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/jobList_task/static/js/2.431facc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439c916b57ab3f7a48ba",
+    "url": "/jobList_task/static/js/main.4b73cc8a.chunk.js"
+  },
+  {
+    "revision": "8c2b8cb058ff5d2c8a03",
+    "url": "/jobList_task/static/js/runtime-main.b88ce54e.js"
+  }
+]);
